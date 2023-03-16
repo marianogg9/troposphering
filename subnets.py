d3578e44b6245149e2a67932ac8dc6e9e8739581
marianogg9 <gonzalez.mariano.gabriel@gmail.com>
2023-03-16 12:50:51 +0100

    added .gitignore + subnets definition script

---
+++ subnets.py
@@ -0,0 +1,39 @@
+import json
+
+from troposphere import ec2
+from troposphere import Ref, Sub, Export
+from troposphere import Template, Output
+
+t = Template()                                                  # Define output template.
+
+with open('subnets_input.json','r') as s:                       # Read input value file.
+    data = json.load(s)
+
+for s in data['subnets']:                                       # Loop over the subnets values definition.
+    region = data['region']
+    availabilityZone = s['availability-zone']
+    name = 'Subnet'+region+availabilityZone
+    subnet = ec2.Subnet(                                        # Define each subnet.
+        name.replace('-',''),
+        AvailabilityZone=s['availability-zone'],
+        CidrBlock=s['cidr'],
+        VpcId=data['vpcid'],
+        Tags=[                                                  # Let's combine "local" specific tags
+            {
+                "Key": "Name", 
+                "Value": 'subnet-'+data['region']+s['availability-zone']
+            }
+        ] + [tag for tag in data['common_tags']]                # with global ones.
+    )
+    t.add_resource(subnet)
+
+    output = Output(                                            # Add outputs
+        name.replace('-',''),
+        Value=Ref(subnet),
+        Export=Export(Sub("${AWS::StackName}-" + subnet.title)) # and export values.
+    )
+    
+    t.add_output(output)
+
+with open('subnets_template.yaml', 'w') as f:                   # Lastly, create the output template file.
+    f.write(t.to_yaml())
